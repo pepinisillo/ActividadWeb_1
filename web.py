5aa7e8e0b72e5fa055b14194e6be009826d96768
Alejandro Suárez Sandoval <126518765+pepinisillo@users.noreply.github.com>
2026-02-16 04:36:41 +0000

    Home Page

--- web.py
+++ web.py
@@ -10,16 +10,31 @@ class WebRequestHandler(BaseHTTPRequestHandler):
         return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
+        if self.url().path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            with open("home.html", "r", encoding="utf-8") as archivo:
+                html = archivo.read()
+            self.wfile.write(html.encode("utf-8"))
+            return #Dejar de ejecutar el resto del código
+        else:
+            self.send_error(404, "Página no encontrada")
+            return #Mostrar error en caso de que la ruta sea una distinta 
+
+        """
+        if self.query_data().get("autor") is None:
+            self.send_error(400, "No se proporcionó el autor")
+            return
+
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
         self.wfile.write(self.get_response().encode("utf-8"))
+        """
 
     def get_response(self):
-        if self.query_data().get("autor") is None:
-            return self.send_error(400, "No se proporcionó el autor")
-        else:
-            return f"""
+        return f"""
         <h1> Proyecto: {self.url().path.split('/')[-1]} Autor: {self.query_data()['autor']}</h1>
     """
 
